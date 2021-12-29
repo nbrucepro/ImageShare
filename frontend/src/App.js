eb7b172c272d35d74d0643260e943ddc4ff4486a
bruce neza <nezabruce@gmail.com>
2021-12-29 10:56:45 +0200

    but

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     hi bruce why this happen for sure mi people
+     hi bruce why this happen for sure mi people but
     </div>
   );
 }
