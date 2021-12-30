7a2437db7af525c989b48d1925b43d9330fdb0c1
bruce neza <nezabruce@gmail.com>
2021-12-30 17:02:02 +0200

    toggleSidebarhome

--- frontend/src/container/Home.js
+++ frontend/src/container/Home.js
@@ -22,26 +22,34 @@ function Home() {
         scrollRef.current.scrollTo(0, 0);
     })
     return (
-      <div className ="flex bg-gray-50 md:flex-row flex-col h-screen transion-height duration-75 ease-out">
-            <div className="hidden md:flex h-screen flex-initial">
-                {/* <Sidebar user={user && user} /> */}
+      <div className="flex bg-gray-50 md:flex-row flex-col h-screen transion-height duration-75 ease-out">
+        <div className="hidden md:flex h-screen flex-initial">
+          {/* <Sidebar user={user && user} /> */}
+        </div>
+        <div className="flex md:hidden flex-row">
+          <div className="p-2 w-full flex flex-row justify-between items-center shadow-md">
+            <HiMenu
+              fontSize={40}
+              className="cursor-pointer"
+              onClick={() => setToggleSidebar(true)}
+            />
+            <Link to="/">
+              {/* {logo} */}
+              <img src="/" alt="logo" className="w-28" />
+            </Link>
+            <Link to={`user-profile/${user?._id}`}>
+              {/* {user?.image} */}
+              <img src="/" alt="suer-pic" className="w-9 h-9 rounded-full" />
+            </Link>
+          </div>
+          {toggleSidebar && (
+            <div className="fixed w-4/5 bg-white h-screen overflow-y-auto shadow-md z-10 animate-slide-in">
+            <div className="absolute w-full flex justify-end items-center p-2">
+            <AiFillCloseCircle fontSize={30} className="cursor-pointer" onClick={() => setToggleSidebar(false)} />                
+              </div>
             </div>
-            <div className="flex md:hidden flex-row">
-    <div className="p-2 w-full flex flex-row justify-between items-center shadow-md">
-                    <HiMenu fontSize={40} className="cursor-pointer" onClick={() => setToggleSidebar(true)} />
-                    <Link to="/">
-                        {/* {logo} */}
-                        <img src='/' alt="logo" className="w-28"/>
-                    </Link>
-                    <Link to={`user-profile/${user?._id}`}>
-                        {/* {user?.image} */}
-                        <img src='/' alt="suer-pic" className="w-9 h-9 rounded-full"/>
-                    </Link>
-                </div>
-                {toggleSidebar && (
-                    <div></div>
-                )}
-            </div>
+          )}
+        </div>
       </div>
     );
 }
