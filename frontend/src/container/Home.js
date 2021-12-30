c08e1172b23f644256d48496718c0106cca2e1b9
bruce neza <nezabruce@gmail.com>
2021-12-30 16:42:29 +0200

    HiMenu

--- frontend/src/container/Home.js
+++ frontend/src/container/Home.js
@@ -23,8 +23,13 @@ function Home() {
     })
     return (
       <div className ="flex bg-gray-50 md:flex-row flex-col h-screen transion-height duration-75 ease-out">
-            <div className="hidden.md:flex.h-screen.flex-initial">
+            <div className="hidden md:flex h-screen flex-initial">
                 {/* <Sidebar user={user && user} /> */}
+            </div>
+            <div className="flex md:hidden flex-row">
+    <div className="p-2 w-full flex flex-row justify-between items-center shadow-md">
+    <HiMenu fontSize={40} className="cursor-pointer" onClick={() => setToggleSidebar(true)} />                
+</div>
             </div>
       </div>
     );
