a12479d5ef07894ad7d23ceb87f87bfb17075b3c
bruce neza <nezabruce@gmail.com>
2021-12-30 16:25:11 +0200

    homeimport

---
+++ frontend/src/container/Home.js
@@ -0,0 +1,19 @@
+import React, { useState, useRef, useEffect } from "react";
+import { HiMenu } from "react-icons/hi";
+import { AiFillCloseCircle } from "react-icons/ai";
+import { Link, Route, Routes } from "react-router-dom";
+
+// import { Sidebar, UserProfile } from "../components";
+// import { userQuery } from "../utils/data";
+// import { client } from "../client";
+// import Pins from "./Pins";
+// import logo from "../assets/logo.png";
+function Home() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Home
