ea32fc9083edda00d268fd165add284f8f82e714
bruce neza <nezabruce@gmail.com>
2021-12-31 00:49:16 +0200

    pinimport

---
+++ frontend/src/container/Pins.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import React, { useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+import { Navbar, Feed, PinDetail, CreatePin, Search } from '../components';
+
+function Pins() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Pins
