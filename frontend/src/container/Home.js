c8c7144206aca4fbd9b3e2a419eae4df85e54a51
bruce neza <nezabruce@gmail.com>
2021-12-30 17:07:16 +0200

    Sidebarhome

--- frontend/src/container/Home.js
+++ frontend/src/container/Home.js
@@ -46,7 +46,8 @@ function Home() {
             <div className="fixed w-4/5 bg-white h-screen overflow-y-auto shadow-md z-10 animate-slide-in">
             <div className="absolute w-full flex justify-end items-center p-2">
             <AiFillCloseCircle fontSize={30} className="cursor-pointer" onClick={() => setToggleSidebar(false)} />                
-              </div>
+                        </div>
+                              <Sidebar closeToggle={setToggleSidebar} user={user && user} />
             </div>
           )}
         </div>
