7d7242da2902ba2c7114b65efe819c44af01d95a
bruce neza <nezabruce@gmail.com>
2021-12-30 16:19:52 +0200

    Routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
+import React, { useEffect } from "react";
+import { Routes, Route, useNavigate } from "react-router-dom";
 
+import { Login } from "./components";
+import Home from "./container/Home.js";
 function App() {
+  const navigate = useNavigate();
+  useEffect(() => {
+    const user = localStorage.getItem('user') !== 'undefined' ? JSON.parse(localStorage.getItem('user')) : localStorage.clear();
+    if (!user) navigate('login');
+  }, []);
   return (
-    <div className="App">
-     hi bruce why this happen for sure mi people but who knows
-    </div>
+    <Routes>
+      <div className="App"></div>
+    </Routes>
   );
 }
 
