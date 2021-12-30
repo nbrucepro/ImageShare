3bbd15e2b2b72965a756260c55f3630d5d8d7db9
bruce neza <nezabruce@gmail.com>
2021-12-31 01:09:14 +0200

    ActiveStylesidebar

--- frontend/src/components/SideBar.js
+++ frontend/src/components/SideBar.js
@@ -5,6 +5,8 @@ import { IoIosArrowForward } from "react-icons/io";
 import logo from "../assets/logo.png";
 import { categories } from "../utils/data";
 
+const isNotActiveStyle = 'flex items-center px-5 gap-3 text-gray-500 hover:text-black transition-all duration-200 ease-in-out capitalize';
+const isActiveStyle = 'flex items-center px-5 gap-3 font-extrabold border-r-2 border-black  transition-all duration-200 ease-in-out capitalize';
 const SideBar = () => {
   return <div></div>;
 };
