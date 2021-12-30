d79f6edf166d3b9e1293bbba3c94e8f53450c88d
bruce neza <nezabruce@gmail.com>
2021-12-31 01:11:54 +0200

    HomeLinkidebar

--- frontend/src/components/SideBar.js
+++ frontend/src/components/SideBar.js
@@ -11,7 +11,41 @@ const SideBar = ({closeToggle,user}) => {
       const handleCloseSidebar = () => {
         if (closeToggle) closeToggle(false);
       };
-  return <div></div>;
+    return (
+    <div className="flex flex-col justify-between bg-white h-full overflow-y-scroll min-w-210 hide-scrollbar">
+      <div className="flex flex-col">
+        <Link
+          to="/"
+          className="flex px-5 gap-2 my-6 pt-1 w-190 items-center"
+          onClick={handleCloseSidebar}
+        >
+          <img src={logo} alt="logo" className="w-full" />
+        </Link>
+        <div className="flex flex-col gap-5">
+
+          <NavLink
+            to="/"
+            className={({ isActive }) => (isActive ? isActiveStyle : isNotActiveStyle)}
+            onClick={handleCloseSidebar}
+          >
+            <RiHomeFill />
+            Home
+          </NavLink>
+          <h3 className="mt-2 px-5 text-base 2xl:text-xl">Discover cateogries</h3>
+          {categories.slice(0, categories.length - 1).map((category) => (
+            <NavLink
+              to={`/category/${category.name}`}
+              className={({ isActive }) => (isActive ? isActiveStyle : isNotActiveStyle)}
+              onClick={handleCloseSidebar}
+              key={category.name}
+            >
+              <img src={category.image} className="w-8 h-8 rounded-full shadow-sm" />
+              {category.name}
+            </NavLink>
+          ))}
+        </div>
+      </div>
+        );
 };
 
 export default SideBar;
