67a88c96bdd6f8bae5fb16bd196e11569bd41e31
bruce neza <nezabruce@gmail.com>
2021-12-31 00:55:20 +0200

    pinsearch

--- frontend/src/container/Pins.js
+++ frontend/src/container/Pins.js
@@ -3,8 +3,15 @@ import { Routes, Route } from "react-router-dom";
 
 import { Navbar, Feed, PinDetail, CreatePin, Search } from "../components";
 
-function Pins() {
-  return <div></div>;
+function Pins({user}) {
+  const [searchTerm, setSearchTerm] = useState("");  
+    return (
+  <div className="px-2 md:px-5">
+      <div className="bg-gray-50">
+        <Navbar searchTerm={searchTerm} setSearchTerm={setSearchTerm} user={user && user} />
+            </div>
+            </div>
+        )
 }
 
 export default Pins;
