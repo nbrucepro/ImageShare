631e5ae804b59aabd5493646f1f672ccbbb30450
bruce neza <nezabruce@gmail.com>
2021-12-31 01:05:31 +0200

    sidebarimports

---
+++ frontend/src/components/SideBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { NavLink, Link } from "react-router-dom";
+import { RiHomeFill } from "react-icons/ri";
+import { IoIosArrowForward } from "react-icons/io";
+import logo from "../assets/logo.png";
+import { categories } from "../utils/data";
+
+const SideBar = () => {
+  return <div></div>;
+};
+
+export default SideBar;
