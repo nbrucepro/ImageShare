ff89110bb8bcca0ab308ea7e96e3ab41f7f52196
bruce neza <nezabruce@gmail.com>
2021-12-30 23:50:43 +0200

    importuserporfile

---
+++ frontend/src/components/UserProfile.js
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from "react";
+import { AiOutlineLogout } from "react-icons/ai";
+import { useParams, useNavigate } from "react-router-dom";
+import { GoogleLogout } from "react-google-login";
+
+import {userCreatedPinsQuery,userQuery,userSavedPinsQuery,} from "../utils/data";
+import { client } from "../client";
+import MasonryLayout from "./MasonryLayout";
+import Spinner from "./Spinner";
+
+const activeBtnStyles =
+  "bg-red-500 text-white font-bold p-2 rounded-full w-20 outline-none";
+const notActiveBtnStyles =
+  "bg-primary mr-4 text-black font-bold p-2 rounded-full w-20 outline-none";
+
+const UserProfile = () => {
+  return <div></div>;
+};
+
+export default UserProfile;
