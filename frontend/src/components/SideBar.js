2e924e658e5e2fe6e94d747428960e5c1a229d2f
bruce neza <nezabruce@gmail.com>
2021-12-31 01:10:15 +0200

    handleCloseSidebar

--- frontend/src/components/SideBar.js
+++ frontend/src/components/SideBar.js
@@ -7,7 +7,10 @@ import { categories } from "../utils/data";
 
 const isNotActiveStyle = 'flex items-center px-5 gap-3 text-gray-500 hover:text-black transition-all duration-200 ease-in-out capitalize';
 const isActiveStyle = 'flex items-center px-5 gap-3 font-extrabold border-r-2 border-black  transition-all duration-200 ease-in-out capitalize';
-const SideBar = () => {
+const SideBar = ({closeToggle,user}) => {
+      const handleCloseSidebar = () => {
+        if (closeToggle) closeToggle(false);
+      };
   return <div></div>;
 };
 
