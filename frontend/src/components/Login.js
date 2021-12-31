88ff167d0e30f45cd1b3a22d7ba7f6122cc56ff0
bruce neza <nezabruce@gmail.com>
2021-12-31 23:07:08 +0200

    loginimport

---
+++ frontend/src/components/Login.js
@@ -0,0 +1,12 @@
+import React from "react";
+import GoogleLogin from "react-google-login";
+import { useNavigate } from "react-router-dom";
+import { FcGoogle } from "react-icons/fc";
+import shareVideo from "../assets/share.mp4";
+import logo from "../assets/logowhite.png";
+
+import { client } from "../client";
+
+
+
+export default Login;
