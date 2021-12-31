a2532e026e9bb4a6fea8b2e44badf03d8af13c92
bruce neza <nezabruce@gmail.com>
2021-12-31 22:42:24 +0200

    searchimport

---
+++ frontend/src/components/Search.js
@@ -0,0 +1,9 @@
+import React, { useEffect, useState } from "react";
+
+import MasonryLayout from "./MasonryLayout";
+import { client } from "../client";
+import { feedQuery, searchQuery } from "../utils/data";
+import Spinner from "./Spinner";
+
+
+export default Search;
