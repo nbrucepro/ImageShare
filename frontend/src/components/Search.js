56aa6a3134777770b9b28725a1d59c4fa7257919
bruce neza <nezabruce@gmail.com>
2021-12-31 22:43:22 +0200

    searchpinloader

--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -25,7 +25,15 @@ const Search = ({ searchTerm }) => {
     }
   }, [searchTerm]);
 
- 
+  return (
+    <div>
+      {loading && <Spinner message="Searching pins" />}
+      {pins?.length !== 0 && <MasonryLayout pins={pins} />}
+      {pins?.length === 0 && searchTerm !== "" && !loading && (
+        <div className="mt-10 text-center text-xl ">No Pins Found!</div>
+      )}
+    </div>
+  );
 };
 
 export default Search;
