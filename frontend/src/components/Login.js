895d9ca9780cac32236673bf589992e3b2ce9cc9
bruce neza <nezabruce@gmail.com>
2021-12-31 23:07:38 +0200

    googlelogin

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -6,7 +6,24 @@ import shareVideo from "../assets/share.mp4";
 import logo from "../assets/logowhite.png";
 
 import { client } from "../client";
+const Login = () => {
+  const navigate = useNavigate();
+  const responseGoogle = (response) => {
+    localStorage.setItem("user", JSON.stringify(response.profileObj));
+    const { name, googleId, imageUrl } = response.profileObj;
+    const doc = {
+      _id: googleId,
+      _type: "user",
+      userName: name,
+      image: imageUrl,
+    };
+    client.createIfNotExists(doc).then(() => {
+      navigate("/", { replace: true });
+    });
+  };
 
+  
+};
 
 
 export default Login;
