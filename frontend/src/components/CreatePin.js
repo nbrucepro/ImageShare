950c30dcc166efd2ddec4b04ff8895499c251fb3
bruce neza <nezabruce@gmail.com>
2021-12-31 23:04:12 +0200

    createPin

--- frontend/src/components/CreatePin.js
+++ frontend/src/components/CreatePin.js
@@ -7,6 +7,80 @@ import { categories } from "../utils/data";
 import { client } from "../client";
 import Spinner from "./Spinner";
 
+const CreatePin = ({ user }) => {
+    const [title, setTitle] = useState("");
+    const [about, setAbout] = useState("");
+    const [loading, setLoading] = useState(false);
+    const [destination, setDestination] = useState();
+    const [fields, setFields] = useState();
+    const [category, setCategory] = useState();
+    const [imageAsset, setImageAsset] = useState();
+    const [wrongImageType, setWrongImageType] = useState(false);
 
+    const navigate = useNavigate();
+
+    const uploadImage = (e) => {
+        const selectedFile = e.target.files[0];
+        // uploading asset to sanity
+        if (
+            selectedFile.type === "image/png" ||
+            selectedFile.type === "image/svg" ||
+            selectedFile.type === "image/jpeg" ||
+            selectedFile.type === "image/gif" ||
+            selectedFile.type === "image/tiff"
+        ) {
+            setWrongImageType(false);
+            setLoading(true);
+            client.assets
+                .upload("image", selectedFile, {
+                    contentType: selectedFile.type,
+                    filename: selectedFile.name,
+                })
+                .then((document) => {
+                    setImageAsset(document);
+                    setLoading(false);
+                })
+                .catch((error) => {
+                    console.log("Upload failed:", error.message);
+                });
+        } else {
+            setLoading(false);
+            setWrongImageType(true);
+        }
+    };
+
+    const savePin = () => {
+        if (title && about && destination && imageAsset?._id && category) {
+            const doc = {
+                _type: "pin",
+                title,
+                about,
+                destination,
+                image: {
+                    _type: "image",
+                    asset: {
+                        _type: "reference",
+                        _ref: imageAsset?._id,
+                    },
+                },
+                userId: user._id,
+                postedBy: {
+                    _type: "postedBy",
+                    _ref: user._id,
+                },
+                category,
+            };
+            client.create(doc).then(() => {
+                navigate("/");
+            });
+        } else {
+            setFields(true);
+
+            setTimeout(() => {
+                setFields(false);
+            }, 2000);
+        }
+    };
+}
 
 export default CreatePin;
