8de08cc978ab3aeaa7b47a432bdfb8f5bcc895e2
bruce neza <nezabruce@gmail.com>
2021-12-31 22:52:52 +0200

    fetchpindetails

--- frontend/src/components/PinDetails.js
+++ frontend/src/components/PinDetails.js
@@ -7,5 +7,31 @@ import { client, urlFor } from "../client";
 import MasonryLayout from "./MasonryLayout";
 import { pinDetailMorePinQuery, pinDetailQuery } from "../utils/data";
 import Spinner from "./Spinner";
+const PinDetail = ({ user }) => {
+  const { pinId } = useParams();
+  const [pins, setPins] = useState();
+  const [pinDetail, setPinDetail] = useState();
+  const [comment, setComment] = useState("");
+  const [addingComment, setAddingComment] = useState(false);
+
+  const fetchPinDetails = () => {
+    const query = pinDetailQuery(pinId);
+
+    if (query) {
+      client.fetch(`${query}`).then((data) => {
+        setPinDetail(data[0]);
+        console.log(data);
+        if (data[0]) {
+          const query1 = pinDetailMorePinQuery(data[0]);
+          client.fetch(query1).then((res) => {
+            setPins(res);
+          });
+        }
+      });
+    }
+  };
+
+};
+
 
 export default PinDetail;
