8f7c14b51af8bb365b589e2e39ffb4dd6512a8f7
bruce neza <nezabruce@gmail.com>
2021-12-31 22:43:08 +0200

    searchuseffect

--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -5,5 +5,27 @@ import { client } from "../client";
 import { feedQuery, searchQuery } from "../utils/data";
 import Spinner from "./Spinner";
 
+const Search = ({ searchTerm }) => {
+  const [pins, setPins] = useState();
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    if (searchTerm !== "") {
+      setLoading(true);
+      const query = searchQuery(searchTerm.toLowerCase());
+      client.fetch(query).then((data) => {
+        setPins(data);
+        setLoading(false);
+      });
+    } else {
+      client.fetch(feedQuery).then((data) => {
+        setPins(data);
+        setLoading(false);
+      });
+    }
+  }, [searchTerm]);
+
+ 
+};
 
 export default Search;
