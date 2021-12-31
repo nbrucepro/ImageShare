295c7722bbdbc8146537d41258e282c453a90734
bruce neza <nezabruce@gmail.com>
2021-12-31 23:34:30 +0200

    datautilsuserCreatedPinsQuery

--- frontend/src/utils/data.js
+++ frontend/src/utils/data.js
@@ -183,3 +183,28 @@ export const userQuery = (userId) => {
   return query;
 };
 
+export const userCreatedPinsQuery = (userId) => {
+  const query = `*[ _type == 'pin' && userId == '${userId}'] | order(_createdAt desc){
+    image{
+      asset->{
+        url
+      }
+    },
+    _id,
+    destination,
+    postedBy->{
+      _id,
+      userName,
+      image
+    },
+    save[]{
+      postedBy->{
+        _id,
+        userName,
+        image
+      },
+    },
+  }`;
+  return query;
+};
+
