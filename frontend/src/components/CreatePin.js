a7ef0f7fc392919c8c2bf80c962083d7a107e5fb
bruce neza <nezabruce@gmail.com>
2021-12-31 23:02:18 +0200

    createpinimport

---
+++ frontend/src/components/CreatePin.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import { AiOutlineCloudUpload } from "react-icons/ai";
+import { useNavigate } from "react-router-dom";
+import { MdDelete } from "react-icons/md";
+
+import { categories } from "../utils/data";
+import { client } from "../client";
+import Spinner from "./Spinner";
+
+
+
+export default CreatePin;
