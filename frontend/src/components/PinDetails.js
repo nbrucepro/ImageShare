84e21175aacde904641c61c24acf409af9c9b03e
bruce neza <nezabruce@gmail.com>
2021-12-31 22:51:11 +0200

    pindetailinport

---
+++ frontend/src/components/PinDetails.js
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from "react";
+import { MdDownloadForOffline } from "react-icons/md";
+import { Link, useParams } from "react-router-dom";
+import { v4 as uuidv4 } from "uuid";
+
+import { client, urlFor } from "../client";
+import MasonryLayout from "./MasonryLayout";
+import { pinDetailMorePinQuery, pinDetailQuery } from "../utils/data";
+import Spinner from "./Spinner";
+
+export default PinDetail;
