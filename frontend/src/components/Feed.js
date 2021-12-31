9d805f212213d322c570b6b504a515e4130ad1d1
bruce neza <nezabruce@gmail.com>
2021-12-31 22:48:12 +0200

    feedquery

--- frontend/src/components/Feed.js
+++ frontend/src/components/Feed.js
@@ -5,3 +5,29 @@ import { client } from "../client";
 import { feedQuery, searchQuery } from "../utils/data";
 import MasonryLayout from "./MasonryLayout";
 import Spinner from "./Spinner";
+const Feed = () => {
+  const [pins, setPins] = useState();
+  const [loading, setLoading] = useState(false);
+  const { categoryId } = useParams();
+
+  useEffect(() => {
+    if (categoryId) {
+      setLoading(true);
+      const query = searchQuery(categoryId);
+      client.fetch(query).then((data) => {
+        setPins(data);
+        setLoading(false);
+      });
+    } else {
+      setLoading(true);
+
+      client.fetch(feedQuery).then((data) => {
+        setPins(data);
+        setLoading(false);
+      });
+    }
+  }, [categoryId]);
+  const ideaName = categoryId || "new";
+  };
+
+export default Feed;
