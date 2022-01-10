7e78b9c8099e205a75474a3304057c5cccf17ed6
bruce neza <nezabruce@gmail.com>
2022-01-10 23:29:44 +0200

    PYUOUOIOIJ

--- backend/schemas/schema.js
+++ backend/schemas/schema.js
@@ -5,7 +5,7 @@ import createSchema from "part:@sanity/base/schema-creator";
 import schemaTypes from "all:part:@sanity/base/schema-type";
 import pin from "./pin";///NNNNNN
 import user from "./user";
-import comment from "./comment";  //amahoroo
+import comment from "./comment";  //amahoroo//PYUOUOIOIJ
 import postedBy from "./postedBy";//nomnomnoms
 import save from "./save";
 //yop
