b4a9418a3096d4f9a2bfdbd0ca415655eb77fa8c
bruce neza <nezabruce@gmail.com>
2022-01-13 13:43:22 +0200

    ik

---
+++ Mailer/server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+
+const nodemailer = require('nodemailer');
+const PORT = process.env.PORT || 5000;
+
+//Middleware
+
+app.use(express.static('public'));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/contactform.html');
+})
+app.post('/', (req, res) => {
+    console.log(req.body);
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'nezabruce@gmail.com',
+            pass:'e'
+        }
+    })
+    const mailOptions = {
+        from: req.body.email,
+        to:"nezabruce@gmail.com",
+        subject: `Message from ${req.body.email} : ${req.body.subject}`,
+        text:req.body.message
+    }
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.log(error);
+            res.send('error');
+        } else {
+            console.log('Email sent : '+ info.response)
+            res.send('success')
+        }
+    })
+})
+app.listen(PORT, () => {
+    console.log("Hi");
+})
