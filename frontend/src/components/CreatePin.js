66765411726cfe7789dea2769d93b8160b9c0a13
bruce neza <nezabruce@gmail.com>
2022-01-15 21:50:20 +0200

    not

--- frontend/src/components/CreatePin.js
+++ frontend/src/components/CreatePin.js
@@ -82,6 +82,7 @@ const CreatePin = ({ user }) => {
     }
     ///sanity i will come
     //admindashboard
+    //notworking
   };
   return (
     <div className="flex flex-col justify-center items-center mt-5 lg:h-4/5">
