9becddee36f58aa21ed6aa59b2b371070f7c1eb8
NezaBruce <nezabruce@gmail.com>
2022-01-16 23:21:25 +0200

    go

--- frontend/src/components/MasonaryLayout.js
+++ frontend/src/components/MasonaryLayout.js
@@ -10,6 +10,7 @@ const breakpointColumnsObj = {
   1000: 2,
   500: 1,
 };
+//ofofrudeboy
 
  const MasonryLayout = ({ pins }) => (
    <Masonry className="flex animate-slide-fwd" breakpointCols={breakpointColumnsObj}>
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -3,7 +3,7 @@ import { Link, useNavigate } from "react-router-dom";
 import { IoMdAdd, IoMdSearch } from "react-icons/io";
 const Navbar = ({ searchTerm, setSearchTerm, user }) => {
   const navigate = useNavigate();
-
+  const navigate = useNavigate();
   if (user) {
     return (
       <div className="flex gap-2 md:gap-5 w-full mt-5 pb-7 ">
