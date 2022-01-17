6cca4a0c3b4281f93f203529c88b3a9633c387a3
NezaBruce <nezabruce@gmail.com>
2022-01-17 21:14:52 +0200

    could

--- frontend/src/container/Home.js
+++ frontend/src/container/Home.js
@@ -25,7 +25,7 @@ function Home() {
         <div className="hidden md:flex h-screen flex-initial">
           {/* <Sidebar user={user && user} /> */}
           //whathasihejuru
-          //idoknowifI
+          //idoknowifIcould
         </div>
         <div className="flex md:hidden flex-row">
           <div className="p-2 w-full flex flex-row justify-between items-center shadow-md">
