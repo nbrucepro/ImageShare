764c6b893b488656a8ba10bb584b90418ead4e6b
NezaBruce <nezabruce@gmail.com>
2022-01-19 21:43:21 +0200

    nway

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -24,6 +24,12 @@ const Navbar = ({ searchTerm, setSearchTerm, user }) => {
   //tailwindisonway
   //tailwindisonway
   //tailwindisonway
+  //tailwindisonway
+  //tailwindisonway
+  //tailwindisonway
+  //tailwindisonway
+  //tailwindisonway
+  //tailwindisonway
     return (
       <div className="flex gap-2 md:gap-5 w-full mt-5 pb-7 ">
         <div className="flex justify-start items-center w-full px-2 rounded-md bg-white border-none outline-none focus-within:shadow-sm">
