a5de3358a03aedc5bc0960c3b8b8cb8e55262e90
NezaBruce <nezabruce@gmail.com>
2022-01-21 23:27:23 +0200

    ome

--- frontend/src/components/SideBar.js
+++ frontend/src/components/SideBar.js
@@ -65,6 +65,12 @@ const SideBar = ({closeToggle,user}) => {
         //phizezeerwhatitdonetome
         //phizezeerwhatitdonetome
         //phizezeerwhatitdonetome
+        //phizezeerwhatitdonetome
+        //phizezeerwhatitdonetome
+        //phizezeerwhatitdonetome
+        //phizezeerwhatitdonetome
+        //phizezeerwhatitdonetome
+        //phizezeerwhatitdonetome
         if (closeToggle) closeToggle(false);
       };
     return (
