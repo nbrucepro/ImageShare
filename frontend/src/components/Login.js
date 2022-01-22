406b04075735812f6ac28f827ead49e4df70f4b1
NezaBruce <nezabruce@gmail.com>
2022-01-22 23:19:04 +0200

    at

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -24,6 +24,10 @@ const Login = () => {
 //whattotdayittotit
 //whattotdayittotit
 //whattotdayittotit
+//whattotdayittotit
+//whattotdayittotit
+//whattotdayittotit
+//whattotdayittotit
   };
 return (
   <div className="flex justify-start items-center flex-col h-screen">
