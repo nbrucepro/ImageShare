3d1f2efe4bd9b2446d3b52e6ce2eb8d850cb398b
NezaBruce <nezabruce@gmail.com>
2022-01-23 23:10:24 +0200

    re

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -63,6 +63,10 @@ const Login = () => {
 //ipreparedpaint
 //ipreparedpaint
 //ipreparedpaint
+//ipreparedpaint
+//ipreparedpaint
+//ipreparedpaint
+//ipreparedpaint
   };
 return (
   <div className="flex justify-start items-center flex-col h-screen">
