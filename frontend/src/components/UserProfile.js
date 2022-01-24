e87ed33918bc7fe34c2b192f2a7477dfbd04ee67
NezaBruce <nezabruce@gmail.com>
2022-01-24 23:12:05 +0200

    NAV

--- frontend/src/components/UserProfile.js
+++ frontend/src/components/UserProfile.js
@@ -54,6 +54,13 @@ const UserProfile = () => {
   // const navigate = useNavigate();
   // const navigate = useNavigate();
   // const navigate = useNavigate();
+  // const navigate = useNavigate();
+  // const navigate = useNavigate();
+  // const navigate = useNavigate();
+  // const navigate = useNavigate();
+  // const navigate = useNavigate();
+  // const navigate = useNavigate();
+  // const navigate = useNavigate();
     const navigate = useNavigate();
     const { userId } = useParams();
     const User = localStorage.getItem('user') !== 'undefined' ? JSON.parse(localStorage.getItem('user')) : localStorage.clear();
