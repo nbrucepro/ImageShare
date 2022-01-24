3309016cdfa3b255a06bf0e63fec79419b1cc544
NezaBruce <nezabruce@gmail.com>
2022-01-24 23:13:02 +0200

    teTY

--- frontend/src/components/UserProfile.js
+++ frontend/src/components/UserProfile.js
@@ -68,6 +68,10 @@ const UserProfile = () => {
   // const navigate = useNavigate();
   // const navigate = useNavigate();
   // const navigate = useNavigate();
+  // const navigate = useNavigate();
+  // const navigate = useNavigate();
+  // const navigate = useNavigate();
+  // const navigate = useNavigate();
     const navigate = useNavigate();
     const { userId } = useParams();
     const User = localStorage.getItem('user') !== 'undefined' ? JSON.parse(localStorage.getItem('user')) : localStorage.clear();
