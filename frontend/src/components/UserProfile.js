80c11e624daaea08ba1555e540e0534da3a39a04
NezaBruce <nezabruce@gmail.com>
2022-01-25 22:43:02 +0200

    at

--- frontend/src/components/UserProfile.js
+++ frontend/src/components/UserProfile.js
@@ -186,6 +186,11 @@ const UserProfile = () => {
               //godineedhelpfromyouidontknowhatodo
               //godineedhelpfromyouidontknowhatodo
               //godineedhelpfromyouidontknowhatodo
+              //godineedhelpfromyouidontknowhatodo
+              //godineedhelpfromyouidontknowhatodo
+              //godineedhelpfromyouidontknowhatodo
+              //godineedhelpfromyouidontknowhatodo
+              //godineedhelpfromyouidontknowhatodo
             />
           </div>
           <h1 className="font-bold text-3xl text-center mt-3">
