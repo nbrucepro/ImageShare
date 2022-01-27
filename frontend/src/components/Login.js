602409c28acba17f242896b0f7d61c6154172d09
NezaBruce <nezabruce@gmail.com>
2022-01-27 23:18:55 +0200

    su

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -147,6 +147,13 @@ const Login = () => {
 //Godwhatagasuzugurono
 //Godwhatagasuzugurono
 //Godwhatagasuzugurono
+//Godwhatagasuzugurono
+//Godwhatagasuzugurono
+//Godwhatagasuzugurono
+//Godwhatagasuzugurono
+//Godwhatagasuzugurono
+//Godwhatagasuzugurono
+//Godwhatagasuzugurono
   };
 return (
   <div className="flex justify-start items-center flex-col h-screen">
