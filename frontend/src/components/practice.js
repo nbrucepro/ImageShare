ce5a2ec29dd000fdc380ff807afdba465ef8ef48
NezaBruce <nezabruce@gmail.com>
2022-01-31 23:43:22 +0200

    co

--- frontend/src/components/practice.js
+++ frontend/src/components/practice.js
@@ -1,25 +1,25 @@
 function fin(ops) {
     var result = 0;
   let score = [];
-  for (let i = 0; i < score.length; i++) {
+  for (let i = 0; i < ops.length; i++) {
     if (!isNaN(ops[i])) {
-      score.push(ops[i]);
+      score.push(parseInt(ops[i]));
     } else if (ops[i] == "+") {
-      score.push(parseInt(score.length - 2) + parseInt(score.length - 1));
+      score.push(parseInt(score[score.length - 2]) + parseInt(score[score.length - 1]));
     } else if (ops[i] == "D") {
-      score.push(2 * parseInt(score.length - 2));
+      score.push(2 * parseInt(score[score.length - 1]));
     } else if (ops[i] == "C") {
       score.pop();
     } else {
       console.log("undefined");
     }
     }
-     result = score.reduce((a, b) => {
-         
-        a + b
-     }, 0);
+     result = score.reduce((a, b) => a + b, 0);
     return result;
-    // return result;
-    // return result;
 }
-console.log(fin(["1", "2"]));
+// console.log(fin(["5", "2","C","D","+"]));
+// console.log(fin(["5", "2","C","D","+"]));
+// console.log(fin(["5", "2","C","D","+"]));
+// console.log(fin(["5", "2","C","D","+"]));
+// console.log(fin(["5", "2","C","D","+"]));
+// console.log(fin(["5", "2","C","D","+"]));
