f0813af4c46b7d181326ef781b162dcb3920fc5b
NezaBruce <nezabruce@gmail.com>
2022-01-31 23:44:40 +0200

    lo

--- frontend/src/components/practice.js
+++ frontend/src/components/practice.js
@@ -53,3 +53,14 @@ function fin(ops) {
 // console.log(fin(["5", "2","C","D","+"]));
 // console.log(fin(["5", "2","C","D","+"]));
 // console.log(fin(["5", "2","C","D","+"]));
+// console.log(fin(["5", "2","C","D","+"]));
+// console.log(fin(["5", "2","C","D","+"]));
+// console.log(fin(["5", "2","C","D","+"]));
+// console.log(fin(["5", "2","C","D","+"]));
+// console.log(fin(["5", "2","C","D","+"]));
+// console.log(fin(["5", "2","C","D","+"]));
+// console.log(fin(["5", "2","C","D","+"]));
+// console.log(fin(["5", "2","C","D","+"]));
+// console.log(fin(["5", "2","C","D","+"]));
+// console.log(fin(["5", "2","C","D","+"]));
+// console.log(fin(["5", "2","C","D","+"]));
