70b6d1ff3b302083252f36e103d11666c6e07246
NezaBruce <nezabruce@gmail.com>
2022-02-01 21:55:07 +0200

    do

--- frontend/src/components/Spinner.js
+++ frontend/src/components/Spinner.js
@@ -35,3 +35,13 @@ export default Spinner
 //what do you mean aha do you mean at this level of manufacturing of items and other items at some level
 //what do you mean aha do you mean at this level of manufacturing of items and other items at some level
 //what do you mean aha do you mean at this level of manufacturing of items and other items at some level
+//what do you mean aha do you mean at this level of manufacturing of items and other items at some level
+//what do you mean aha do you mean at this level of manufacturing of items and other items at some level
+//what do you mean aha do you mean at this level of manufacturing of items and other items at some level
+//what do you mean aha do you mean at this level of manufacturing of items and other items at some level
+//what do you mean aha do you mean at this level of manufacturing of items and other items at some level
+//what do you mean aha do you mean at this level of manufacturing of items and other items at some level
+//what do you mean aha do you mean at this level of manufacturing of items and other items at some level
+//what do you mean aha do you mean at this level of manufacturing of items and other items at some level
+//what do you mean aha do you mean at this level of manufacturing of items and other items at some level
+//what do you mean aha do you mean at this level of manufacturing of items and other items at some level
