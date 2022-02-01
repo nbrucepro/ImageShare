e951ecc5964134df4ce566b5908f5e30afddc956
NezaBruce <nezabruce@gmail.com>
2022-02-01 21:49:21 +0200

    whw

--- frontend/src/components/Spinner.js
+++ frontend/src/components/Spinner.js
@@ -17,3 +17,9 @@ function Spinner() {
 //pti
 
 export default Spinner
+//what do you mean aha do you mean at this level of manufacturing of items and other items at some level
+//what do you mean aha do you mean at this level of manufacturing of items and other items at some level
+//what do you mean aha do you mean at this level of manufacturing of items and other items at some level
+//what do you mean aha do you mean at this level of manufacturing of items and other items at some level
+//what do you mean aha do you mean at this level of manufacturing of items and other items at some level
+//what do you mean aha do you mean at this level of manufacturing of items and other items at some level
